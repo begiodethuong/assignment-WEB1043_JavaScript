3fe2e7cfbd3a9ba6e6fd0db989389db470f9d317
duyphongqn01 <41832167+duyphongqn01@users.noreply.github.com>
2022-11-09 11:16:26 +0700

    Update sell.js

--- Js/sell.js
+++ Js/sell.js
@@ -1,3 +1,4 @@
+/*hầu như các function mình đã note hết, nên có hàm gì không hiểu hãy google và note thêm nhá các bro*/
 function doimucgia() {
     var arrGia = document.getElementsByName("gia");
     /*get giá trị của giá ở sell.html*/
